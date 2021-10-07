b405b9453b135738e79c93b97c16074b45a2b429
jero91tjorange <ernesto_19911@gmail.com>
2021-10-07 09:50:26 -0700

    test2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Test1Component } from './test1/test1.component';
+import { Test2Component } from './test2/test2.component';
 
 @NgModule({
-  declarations: [AppComponent, Test1Component],
+  declarations: [AppComponent, Test1Component, Test2Component],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
