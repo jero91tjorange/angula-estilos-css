b93d3bfe23c0307ffc8977a87bd12938cc3d1fb3
jero91tjorange <ernesto19911@gmail.com>
2021-10-09 10:39:27 -0700

    metodo-suscribe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, NgModel } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RxjsComponent } from './rxjs/rxjs.component';
+import { Test1Component } from './test1/test1.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, Test1Component, RxjsComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/rxjs/rxjs.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { of, BehaviorSubject, Subscription } from 'rxjs';
+import { filter, map, delay } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-rxjs',
+  templateUrl: './rxjs.component.html',
+  styleUrls: ['./rxjs.component.css'],
+})
+export class RxjsComponent implements OnInit {
+  //tictok = of([1, 2, 3, 4, 5]);
+  video = 1;
+  tictok = new BehaviorSubject(this.video);
+  personaASub: Subscription;
+
+  constructor() {
+    //Persona A
+    this.personaASub = this.tictok
+      .pipe(
+        filter((s) => s % 2 === 0)
+        /*map((s) => s.join('-')),*/
+        //map((s) => s + 'hola')
+      )
+      .subscribe((v) => {
+        console.log('VIDEO A', v);
+      });
+
+    //Persona B
+    this.tictok.pipe(delay(4000)).subscribe((v) => {
+      console.log('VIDEO B', v);
+    });
+
+    //Persona C
+    this.tictok.subscribe((v) => {
+      console.log('VIDEO C', v);
+    });
+  }
+
+  ngOnInit() {}
+
+  addVideo() {
+    this.video++;
+    this.tictok.next(this.video);
+  }
+
+  eliminarsub() {
+    this.personaASub.unsubscribe();
+  }
+}
