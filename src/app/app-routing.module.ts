a0b1d17382e5bf467bc7692558b41674b28a4f5b
jero91tjorange <ernesto19911@gmail.com>
2021-10-12 09:53:49 -0700

    adicional_final

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Test1Component } from './test1/test1.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'test1',
+    component: Test1Component,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
