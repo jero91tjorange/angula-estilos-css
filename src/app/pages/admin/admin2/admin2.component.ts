2a211402e9fe442b99461354a2306b7c17a31121
jero91tjorange <ernesto19911@gmail.com>
2021-10-11 17:33:09 -0700

    Finalizado

--- src/app/pages/admin/admin2/admin2.component.ts
+++ src/app/pages/admin/admin2/admin2.component.ts
@@ -4,9 +4,9 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-admin2',
   template: `
     <div id="container">
-      <p class="hola">admin2 works fine ygyuguyg</p>
+      <p class="hola">admin2 works fine <strong>{{test1??test2}}</strong></p>
       
-      <button (click)=testParams('Ernesto', 'ramirez')>test</button>
+      <button (click)="testParams()">test</button>
     </div>
  
   `,
@@ -19,15 +19,23 @@ import { Component, OnInit } from '@angular/core';
     background-color:yellow;
     color:blue;
   }
+
+  strong{
+    color:red;
+  }
   `,
   ],
 })
 export class Admin2Component implements OnInit {
+  test1 = null;
+  test2 = 'kristy';
   constructor() {}
   ngOnInit() {}
 
-  public testParams(name: string, lastname: string) {
-    console.log('Nombre: ', name + ' Apellido: ' + lastname);
+  public testParams(name: string, lastname?: string = 'kerberos') {
+    //? se utiliza para marcarlo como parametro opcional y se le puede asignar un valor por defecto.
+    const aux = 'jose'; // ?? Se utilizan para sustituir un valor por otro del lado derecho como se ve en el ejemplo acontinuacion
+    console.log('Nombre: ', name ?? aux + ' Apellido: ' + lastname);
     return name + ' ' + lastname;
   }
 }
---
+++ src/app/shared/exponent.pipe.ts
@@ -0,0 +1,11 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'exponent',
+})
+export class ExponentPipe implements PipeTransform {
+  //transform(value: number, exponent?: number = 1): number
+  transform(value: number, exponent?: number): number {
+    return Math.pow(value, isNaN(exponent) ? 1 :expon);
+  }
+}
